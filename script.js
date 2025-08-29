6fa400f5e68d9b74d169f79319fb5f9ec5d332c8
swathigopavarapu <41499776+swathigopavarapu@users.noreply.github.com>
2025-08-29 10:23:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+function updateTimer() {
+	const now = new Date();
+	const formatted = now.toLocaleString("en-GB", {
+		 year: "numeric",
+         month: "2-digit",
+         day: "2-digit",
+         hour: "2-digit",
+         minute: "2-digit",
+         second: "2-digit",
+         hour12: false
+		 });
+	 document.getElementById("timer").innerText = formatted.replace(",", "");
+}
+setInterval(updateTimer, 1000);
+updateTimer();
